c4ec28abf5322e9c9e92997c557ef7b9da8e3a4f
Marco Milanesi <marco.milanesi.99@gmail.com>
2023-06-03 23:31:22 +0200

    update

--- src/Cascade Optimization/pid_filt_constraints.m
+++ src/Cascade Optimization/pid_filt_constraints.m
@@ -1,4 +1,4 @@
-function [cin,ceq]=pid_filt_constraints(x,P,w_vector,MS,wh,Fh_max,wl,Dl_max,PM_min)
+function [cin,ceq]=pid_filt_constraints(x,P,w_vector,MS,wh,Fh_max,wl,Dl_max,PM_min,insertNotch)
 
 % PID_FILT_CONTRAINTS Calcola i valori dei vincoli non lineari.
 %   x = [Kp,Ki,Kd,Tfd,Tfu]: vettore dei parametri
@@ -7,37 +7,50 @@ function [cin,ceq]=pid_filt_constraints(x,P,w_vector,MS,wh,Fh_max,wl,Dl_max,PM_m
 
 % cin<=0
 % ceq==0
-    
-    ceq=[];
-    
-    Kp=x(1); Ki=x(2); Kd=x(3); Tfd=x(4); Tfu=x(5);
-    
-    s=tf('s');
 
-    C=(Kp+Ki/s+Kd*s/(Tfd*s+1))*1/(Tfu*s+1);
-    
-    C_fr=freqresp(C,w_vector);  % frequency response
-    P_fr=freqresp(P,w_vector);
-    C_fr=C_fr(:);               % reshaping da 1x1xn a nx1
-    P_fr=P_fr(:);               % reshaping da 1x1xn a nx1
-    
-    L_fr=C_fr.*P_fr;
-    S_fr=1./(1+L_fr);   % S=1/(1+L)
-    F_fr=L_fr.*S_fr;    % F=L/(1+L)=L*S
-    DY_fr=P_fr.*S_fr;   % DY=Y/D=P/(1+L)=P*S
+ceq=[];
 
-    cin(1)=max(abs(F_fr(w_vector>wh)))-Fh_max;
-    cin(2)=max(abs(DY_fr(w_vector<wl)))-Dl_max;
-    cin(3)=max(abs(S_fr))-MS;
-    
-    L=P*C;
-    margini=allmargin(L);   % calcolo margini di L
-    
-    if isempty(margini.PMFrequency)    % se L ha margine di fase infinito
-        cin(4)=0;
+Kp=x(1); Ki=x(2); Kd=x(3); Tfd=x(4); Tfu=x(5);
+
+s=tf('s');
+
+C=(Kp+Ki/s+Kd*s/(Tfd*s+1))*1/(Tfu*s+1);
+
+L=P*C;    % funzione d'anello aperto
+
+if insertNotch    
+    [gpeak,fpeak] = getPeakGain(L,[],[300 1000]); % constraint of the frequencies personalize for our system
+    if(gpeak > 1/10)
+        filt=notch(fpeak,1/10/gpeak);
     else
-        cin(4)=PM_min-min(margini.PhaseMargin);    % min(PM)>=PM_min -> PM_min-PM<=0
+        filt=1;
     end
+    C=C*filt;
+end
+
+
+C_fr=freqresp(C,w_vector);  % frequency response
+P_fr=freqresp(P,w_vector);
+C_fr=C_fr(:);               % reshaping da 1x1xn a nx1
+P_fr=P_fr(:);               % reshaping da 1x1xn a nx1
+
+L_fr=C_fr.*P_fr;
+S_fr=1./(1+L_fr);   % S=1/(1+L)
+F_fr=L_fr.*S_fr;    % F=L/(1+L)=L*S
+DY_fr=P_fr.*S_fr;   % DY=Y/D=P/(1+L)=P*S
+
+cin(1)=max(abs(F_fr(w_vector>wh)))-Fh_max;
+cin(2)=max(abs(DY_fr(w_vector<wl)))-Dl_max;
+cin(3)=max(abs(S_fr))-MS;
+
+L=P*C;
+margini=allmargin(L);   % calcolo margini di L
+
+if isempty(margini.PMFrequency)    % se L ha margine di fase infinito
+    cin(4)=0;
+else
+    cin(4)=PM_min-min(margini.PhaseMargin);    % min(PM)>=PM_min -> PM_min-PM<=0
+end
 
 end
 
--- src/Cascade Optimization/pid_filt_cost_function.m
+++ src/Cascade Optimization/pid_filt_cost_function.m
@@ -1,23 +1,37 @@
-function J = pid_filt_cost_function(x,P,wc_des)
+function J = pid_filt_cost_function(x,P,wc_des,insertNotch)
 % PID_FILT_COST_FUNCTION Calcola il valore di una cifra di merito J.
 %   J: cifra di merito
 %   x = [Kp,Ki,Kd,Tfd,Tfu]: vettore dei parametri.
 %   P: modello continuo del processo
 %   wc_des: pulsazione di taglio desiderata
-    
-    Kp=x(1); Ki=x(2); Kd=x(3); Tfd=x(4); Tfu=x(5);
-    
-    s=tf('s');
-    
-    C=(Kp+Ki/s+Kd*s/(Tfd*s+1))*1/(Tfu*s+1);     % controllore PID filtrato
 
-    L=P*C;    % funzione d'anello aperto
-    margini=allmargin(L);    % calcolo margini di L
-    
-    if isempty(margini.PMFrequency)    % se L ha margine di fase infinito
-        J=100;    % pongo J a un valore alto
+Kp=x(1); Ki=x(2); Kd=x(3); Tfd=x(4); Tfu=x(5);
+
+s=tf('s');
+
+C=(Kp+Ki/s+Kd*s/(Tfd*s+1))*1/(Tfu*s+1);     % controllore PID filtrato
+L=P*C;    % funzione d'anello aperto
+
+
+
+if insertNotch    
+    [gpeak,fpeak] = getPeakGain(L,[],[100 1000]); % constraint of the frequencies personalize for our system
+    if(gpeak > 1/10)
+        filt=notch(fpeak,1/10/gpeak);
     else
-        J=(wc_des-margini.PMFrequency(end))^2;    % altrimenti calcolo J
+        filt=1;
     end
+    C=C*filt;
+    L=P*C;
+end
+
+margini=allmargin(L);    % calcolo margini di L
+
+if isempty(margini.PMFrequency)    % se L ha margine di fase infinito
+    J=100;    % pongo J a un valore alto
+else
+    J=(wc_des-margini.PMFrequency(end))^2;    % altrimenti calcolo J
+end
+
 
 end
